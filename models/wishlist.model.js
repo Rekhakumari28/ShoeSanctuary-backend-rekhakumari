d382fa05cec4d81816c47a6a8af69956abe1591a
rekha kumari bheel <134380492+Rekhakumari28@users.noreply.github.com>
2024-12-24 18:38:26 +0530

    modify wishlist model

--- models/wishlist.model.js
+++ models/wishlist.model.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose')
 const wishlistSchema = new mongoose.Schema({
-   product:[ {
-       type: mongoose.Schema.Types.ObjectId,
-       ref: "Product",
-     }]
+  
+  wishlist : [{
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    }
+  }]
+  
+  
 })
 
 const Wishlist = mongoose.model('Wishlist', wishlistSchema)
