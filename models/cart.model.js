88c6c6972ad62230016948c71d43a4514b750811
rekha kumari bheel <134380492+Rekhakumari28@users.noreply.github.com>
2025-01-02 17:18:30 +0530

    modify routes and models

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,26 +1,37 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const orderItem = new mongoose.Schema({
-   quantity: { type: Number, default: 1},
-   product: { type: mongoose.Schema.Types.ObjectId, ref: "Product"}
-})
-
-const cartSchema = new mongoose.Schema({
-   orderProduct:[ {  orderItem }],
-   shippingAddress: { type: mongoose.Schema.Types.ObjectId, ref: "Address" , default : "24/126, new adarsh colony, gulabpura, Rajasthan, India, 311021"},
-   status: { 
+const cartSchema = new mongoose.Schema(
+  {
+    orderItem: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "OrderItem",
+        required: true,
+      },
+    ],
+    shippingAddress: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Address",
+      default: "24/126, new adarsh colony, gulabpura, Rajasthan, India, 311021",
+    },
+    status: {
       type: String,
-      enum:["Pending","Delivered","Canceled"],
-      default: "Pending"
-   },
-   totalPrice: {
+      enum: ["Pending", "Delivered", "Canceled"],
+      default: "Pending",
+    },
+    totalPrice: {
       type: Number,
       required: true,
-      default: 0
-   },
-   user : {type: mongoose.Schema.Types.ObjectId, ref: "User", default : "Rekha Kumari"}
+      default: 0,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: "Rekha Kumari",
+    },
+  },
+  { timestamps: true }
+);
 
-},{timestamps: true})
-
-const Cart = mongoose.model("Cart", cartSchema)
-module.exports = Cart
+const Cart = mongoose.model("Cart", cartSchema);
+module.exports = Cart;
