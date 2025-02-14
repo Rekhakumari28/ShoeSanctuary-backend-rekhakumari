5c3cccec1d5a48cc52631851569a4662d16c4a31
rekha kumari bheel <134380492+Rekhakumari28@users.noreply.github.com>
2025-02-14 15:28:46 +0530

    added mini schema in cart model

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,14 +1,20 @@
 const mongoose = require("mongoose");
 
+const orderItemSchem = new mongoose.Schema({
+  quantity: {
+    type: Number,
+    required: true,
+    default: 1
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+  },
+});
+
 const cartSchema = new mongoose.Schema(
   {
-    orderItems: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "OrderItem",
-        required: true,
-      },
-    ],
+    orderItems: [orderItemSchem],
 
     shippingAddress: {
       type: mongoose.Schema.Types.ObjectId,
